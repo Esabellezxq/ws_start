98a56e27171a19ba89a99c7d1d13ea41027927ef
Esabelle_614 <562401579@qq.com>
2018-08-06 01:03:11 +0800

    all

--- simple.js
+++ simple.js
@@ -1,47 +1,80 @@
 const WebSocket = require('ws');
+const UUID = require('uuid');
 const wss = new WebSocket.Server({ port: 3000 });
 console.log("listening") ;
-let userRoom = {};
+var userRoom = {};
 // 校验用户
-function parseUser(){}
+// function parseUser(){}
 //群主创建新房间并加入
-function createRoom(){
-    let loginRes = {
-        type: 'loginRes',
-        data: 'success'
+function createRoom(wsCon,user){
+    let id = UUID.v1();
+    userRoom[id] = [];
+    userRoom[id].push({
+        con: wsCon,
+        user: user
+    });
+    // console.log(userRoom);
+    let data = {
+        roomId: id,
     }
-    return loginRes;
+    var status = false;
+    if(data){
+        status = true;
+    }
+    return { 
+        status: status,
+        data: data
+    };
 }
+
 //加入已有房间
-function enterRoom(){}
-//退出房间
-function exitRoom(){}
-// 判断是否重复加入房间
-function reEnter(){}
-// var msg = "hello kiki!broadcast test";
-// wss.broadcast = function broadcast(data){
-//     wss.clients.forEach(function each(client){
-//         if(client.readyState === WebSocket.OPEN){
-//             client.send(data);
-//         }
-//     });
-// };
+function enterRoom(wsCon, room , user){
+    userRoom[room] = [] ;
+    userRoom[room].push({
+        con: wsCon,
+        user: user
+    });
+    return userRoom[room];
+}
+// //退出房间
+// function exitRoom(){}
+// // 判断是否重复加入房间
+// function reEnter(){}
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         // console.log('received: %s', message);
-        let loginMsg = JSON.parse(message);
-        switch(loginMsg.type){
-            case 'login':
-                ws.send(JSON.stringify(createRoom()));
+        let clientMsg = JSON.parse(message);
+        let req = clientMsg.data;
+        switch(clientMsg.type){
+            case 'createReq':
+                let createRes = {
+                    type: 'createRes',
+                    data: createRoom(ws,req.user)
+                }
+                ws.send(JSON.stringify(createRes));
                 break;
-            case 'chat':
-                wss.clients.forEach(function each(client){
-                    if(client.readyState === WebSocket.OPEN){
-                        client.send(loginMsg.data);
+            case 'enterReq':
+                enterRoom(ws,req.room,req.user).forEach(function each(client) {
+                    let enterRes = {
+                        type: 'enterRes',
+                        data: `${client.user} 进入房间`
+                    }
+                    if (client.con.readyState === WebSocket.OPEN) {
+                        client.con.send(JSON.stringify(enterRes));
+                        console.log(`${client.user} sent`);
                     }
                 });
-                break;
-        }
+                
+        };
+             
+            // case 'chat':
+            //     wss.clients.forEach(function each(client){
+            //         if(client.readyState === WebSocket.OPEN){
+            //             client.send(loginMsg.data);
+            //         }
+            //     });
+            //     break;
+        // }
         // wss.broadcast(ws.user);
     });
     // ws.send(ws + req.connection);
